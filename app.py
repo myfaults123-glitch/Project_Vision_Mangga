dc993f5be85e0d55bdccc05e9594a1f18bc76168
Anwar Zaidaan <myfaults123@gmail.com>
2026-05-30 16:49:03 +0700

    Update Dockerfile Gajadi Video Analysis

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def index():
 
     return render_template('index.html', result_image=None)
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
